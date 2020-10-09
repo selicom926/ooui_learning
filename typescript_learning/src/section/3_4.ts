43cd3fef40ffdd548359dc5bf00c950492460cc3
ryo yamaoka <selicom926@yahoo.co.jp>
2020-10-09 21:36:45 +0900

    3_4

---
+++ typescript_learning/src/section/3_4.ts
@@ -0,0 +1,16 @@
+//a number
+//b string
+//c 'pineapples'
+//d [boolean, boolean, boolean]×
+//e {type: string}
+//f [number], boolean]×
+//g [number]×
+//h any
+let a = 1024;
+let b = 'apples and oranges';
+const c  = 'pineapples';
+let d = [true, true, false];
+let e = {type: 'ficus'};
+let f = [1, false];
+const g = [3];
+let h = null;
